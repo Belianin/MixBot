4a0ad1135cd9dfbaf2a465d4a47b417e5151ca84
Andrey Belianin <belyaninwork@gmail.com>
2018-09-24 11:55:24 +0500

    Revert "It's alive"
    This reverts commit d26a0e1dd8a5bca84ab63883535d6727bcaaa230.

--- src/Ingredient.java
+++ src/Ingredient.java
@@ -4,11 +4,7 @@ import java.util.List;
 public class Ingredient {
 	public String name;
 	public String ingClass;
-<<<<<<< HEAD
 	public List<Food> possibleFood = new ArrayList<Food>();
-=======
-	public ArrayList<Food> possibleFood;
->>>>>>> 8a5ba77a3213869f03a97cb07c3714d3783dd511
 	
 	public Ingredient(String sName, String sClass, ArrayList<Food> food)
 	{
