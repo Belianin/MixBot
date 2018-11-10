b3d645eb726e6b4e92640b0dcb511a2fb27fe91a
Andrey Belianin <belyaninwork@gmail.com>
2018-11-10 21:06:54 +0500

    Telegram refactoring

--- src/EntryPointTelegram.java
+++ src/EntryPointTelegram.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class EntryPointTelegram extends TelegramLongPollingBot {
 	static MixBot mixBot;
+
 	public static void main(String[] args) {
 		System.out.println("Starting...");
 		mixBot = new MixBot("data/user telegram/");
@@ -26,65 +27,63 @@ public class EntryPointTelegram extends TelegramLongPollingBot {
 		}
 		System.out.println("Settled");
 	}
-	
+
 	@Override
 	public String getBotUsername() {
 		return "IngriBot";
-                //возвращаем юзера
+		// возвращаем юзера
 	}
 
 	@Override
 	public void onUpdateReceived(Update e) {
+		processMessage(e);
+	}
+
+	private void processMessage(Update e) {
+		Message msg = null;
+		String text = null;
+
 		if (e.hasMessage() && e.getMessage().hasText()) {
-		Message msg = e.getMessage();
-		String text = msg.getText();		
-		
+			msg = e.getMessage();
+			text = msg.getText();
+		} else if (e.hasCallbackQuery()) {
+			msg = e.getCallbackQuery().getMessage();
+			text = e.getCallbackQuery().getData();
+		}
+
+		if (msg == null || text == null)
+			return;
+
 		System.out.println(text);
-		//sendMsg(msg, text);
+
 		if (text.equals("/start"))
 			sendMsg(msg, mixBot.initializeSession(msg.getChatId().toString()));
 		else
 			sendMsg(msg, mixBot.respond(msg.getChatId().toString(), text));
-		}
-		else if (e.hasCallbackQuery())
-		{
-			EditMessageText msg = new EditMessageText()
-                    .setChatId(e.getCallbackQuery().getMessage().getChatId())
-                    .setMessageId(e.getCallbackQuery().getMessage().getMessageId())
-                    .setText(e.getCallbackQuery().getData());
-			try {
-				execute(msg);
-			} catch (TelegramApiException ex) {
-				ex.printStackTrace();
-			}
-		}
-		
 	}
-	
+
 	public void sendMsg(Message msg, String text) {
 		SendMessage s = new SendMessage();
 		s.setChatId(msg.getChatId());
 		s.setText(text);
 		s.setReplyMarkup(getMarkup());
-		
+
 		try {
 			execute(s);
 		} catch (TelegramApiException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	private InlineKeyboardMarkup getMarkup()
-	{
+
+	private InlineKeyboardMarkup getMarkup() {
 
 		InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
 		List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
-		
 
 		InlineKeyboardButton button = new InlineKeyboardButton();
 		button.setText("КНУПОЧКА");
 		button.setCallbackData("test_Back");
-		
+
 		keyboard.add(Arrays.asList(button));
 		markup.setKeyboard(keyboard);
 		return markup;
--- src/Response.java
+++ src/Response.java
@@ -1,7 +1,9 @@
+import java.util.List;
 
 public class Response {
 	public String message;
 	public Dialog nextDialog = null;
+	public List<String> buttons;
 
 	public Response(String msg, Dialog next) {
 		message = msg;
