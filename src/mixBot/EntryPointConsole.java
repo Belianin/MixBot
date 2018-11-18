60216359cd7c428ab12e70da2b15d2e4514bfabb
AgaFFon <43129104+AgaFFon@users.noreply.github.com>
2018-11-18 22:34:53 +0500

    deploy requirements

--- src/mixBot/EntryPointConsole.java
+++ src/mixBot/EntryPointConsole.java
@@ -4,24 +4,24 @@ import java.util.Scanner;
 public class EntryPointConsole {
 	private static Scanner input = new Scanner(System.in);
 	
-	public static void main(String[] args) {
-		MixBot mixBot = new MixBot("data/user/");
-		System.out.println("Как вас зовут?");
-		String name = input.nextLine();
-		System.out.println(mixBot.initializeSession(name));
-		while (true)
-		{			
-			String request = getWords();
-			if (request.equals("пока") || request.equals("убейся"))
-                break;
-			String response = mixBot.respond(name, request).message;
-			if (response != null)
-				System.out.println(response);
-		}
-		System.out.println("До свидания!");
-		//MixBot.finishSession(name);
-		mixBot.saveUsers();
-	}
+//	public static void main(String[] args) {
+//		MixBot mixBot = new MixBot("data/user/");
+//		System.out.println("Как вас зовут?");
+//		String name = input.nextLine();
+//		System.out.println(mixBot.initializeSession(name));
+//		while (true)
+//		{
+//			String request = getWords();
+//			if (request.equals("пока") || request.equals("убейся"))
+//                break;
+//			String response = mixBot.respond(name, request).message;
+//			if (response != null)
+//				System.out.println(response);
+//		}
+//		System.out.println("До свидания!");
+//		//MixBot.finishSession(name);
+//		mixBot.saveUsers();
+//	}
 
 	public static String getWords() {
 		return input.nextLine();
